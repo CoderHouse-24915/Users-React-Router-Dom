44766ef6e090267ccddeba8bdb9d4428e5908854
DanielDiSalvo <danieldi_salvo@hotmail.com>
2022-02-08 13:39:15 -0300

    update

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -6,28 +6,39 @@ import './CharacterList.css';
 import { Link } from 'react-router-dom';
 
 import CharacterCard from '../CharacterCard/CharacterCard';
+import Spinner from '../Spinner/Spinner';
 
 const CharacterList = () => {
 	const [characters, setCharacters] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		axios('https://breakingbadapi.com/api/characters').then((res) =>
 			setCharacters(res.data)
 		);
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 1000);
 	}, []);
 
 	return (
-		<div className='CharacterList-container'>
-			{characters.map((char) => {
-				return (
-					<div key={char.char_id}>
-						<Link to={`/detail/${char.char_id}`} className='Link'>
-							<CharacterCard data={char} />
-						</Link>
-					</div>
-				);
-			})}
-		</div>
+		<>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<div className='CharacterList-container'>
+					{characters.map((char) => {
+						return (
+							<div key={char.char_id}>
+								<Link to={`/detail/${char.char_id}`} className='Link'>
+									<CharacterCard data={char} />
+								</Link>
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</>
 	);
 };
 
--- src/views/CharacterDetail/CharacterDetail.js
+++ src/views/CharacterDetail/CharacterDetail.js
@@ -3,33 +3,44 @@ import axios from 'axios';
 import { useParams } from 'react-router';
 import './CharacterDetail.css';
 import CharacterCard from '../../components/CharacterCard/CharacterCard';
+import Spinner from '../../components/Spinner/Spinner';
 
 const CharacterDetail = () => {
 	const [character, setCharacter] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-	console.log(character);
+	// console.log(character);
 
 	let id = useParams();
 
 	let userID = id.id;
 
-	console.log(userID);
+	// console.log(userID);
 
 	useEffect(() => {
 		axios(`https://breakingbadapi.com/api/characters/${userID}`).then((res) =>
 			setCharacter(res.data)
 		);
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 1000);
 	}, [userID]);
 
 	return (
-		<div className='CharacterList-detail'>
-			{character.map((char) => {
-				return (
-					<div key={char.char_id}>
-						<CharacterCard data={char} />
-					</div>
-				);
-			})}
+		<div className='CharacterList-Container'>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<div className='CharacterList-detail'>
+					{character.map((char) => {
+						return (
+							<div key={char.char_id}>
+								<CharacterCard data={char} />
+							</div>
+						);
+					})}
+				</div>
+			)}
 		</div>
 	);
 };
